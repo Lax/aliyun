98c9b6ed1ada8207c1cbd8ae20fa507c99be11dd
Liu Lantao <liulantao@gmail.com>
2015-07-01 14:48:12 +0800

    Add support for aliyun RDS

--- lib/aliyun/services.rb
+++ lib/aliyun/services.rb
@@ -45,8 +45,32 @@ module Aliyun
     end
   end
 
+  class RDSConfig < APIConfig
+    def self.info
+      "Aliyu RDS Service"
+    end
+    def self.endpoint
+      'https://rds.aliyuncs.com/'
+    end
+    def self.default_parameters
+      {
+        :Format=>"JSON",
+        :Version=>"2014-08-15",
+        :SignatureMethod=>"HMAC-SHA1",
+        :SignatureVersion=>"1.0"
+      }
+    end
+    def self.separator
+      super
+    end
+    def self.http_method
+      super
+    end
+  end
+
   SERVICES = {
     :ecs => ECSConfig,
-    :slb => SLBConfig
+    :slb => SLBConfig,
+    :rds => RDSConfig
   }
 end
