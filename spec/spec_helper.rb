8456ad6a0e57f72d32104952d790359abd5bc652
Liu Lantao <liulantao@gmail.com>
2015-07-05 08:22:32 +0800

    spec: load options from ENV

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,6 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'aliyun'
 require 'yaml'
 
-def load_options(src='.aliyun.yml')
-  YAML.load_file(src)
+def load_options
+  {:access_key_id => ENV['ACCESS_KEY_ID'], :access_key_secret => ENV['ACCESS_KEY_SECRET']}
 end
