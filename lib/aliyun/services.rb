b022972ea5ebdbaefc726d3bb23e817e335597c2
Liu Lantao <liulantao@gmail.com>
2015-07-11 00:56:35 +0800

    Add support for aliyun CDN

--- lib/aliyun/services.rb
+++ lib/aliyun/services.rb
@@ -68,9 +68,33 @@ module Aliyun
     end
   end
 
+  class CDNConfig < APIConfig
+    def self.info
+      "Aliyu CDN Service"
+    end
+    def self.endpoint
+      'https://cdn.aliyuncs.com/'
+    end
+    def self.default_parameters
+      {
+        :Format=>"JSON",
+        :Version=>"2014-11-11",
+        :SignatureMethod=>"HMAC-SHA1",
+        :SignatureVersion=>"1.0"
+      }
+    end
+    def self.separator
+      super
+    end
+    def self.http_method
+      super
+    end
+  end
+
   SERVICES = {
     :ecs => ECSConfig,
     :slb => SLBConfig,
-    :rds => RDSConfig
+    :rds => RDSConfig,
+    :cdn => CDNConfig
   }
 end
