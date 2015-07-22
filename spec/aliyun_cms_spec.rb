7ce0ba0a9f4262d7f8782aefec8b8f274bae7396
Liu Lantao <liulantao@gmail.com>
2015-07-22 23:03:37 +0800

    add test case for aliyun CMS

--- spec/aliyun_cms_spec.rb
+++ spec/aliyun_cms_spec.rb
@@ -9,4 +9,24 @@ describe Aliyun do
     expect(service).to be_instance_of(Aliyun::Service)
     expect(service.service).to be(Aliyun::CMSConfig)
   end
+
+  it 'can describe metric datum' do
+    options = load_options
+    options[:service] = :cms
+    service = Aliyun::Service.new options
+    instanceId = load_ecs_instance_id
+    instanceRegion = load_ecs_instance_region
+    parameters = {
+      Namespace: 'acs/ecs',
+      MetricName: 'vm.DiskUtilization',
+      StartTime: (Time.now - 180).utc.iso8601,
+      Dimensions: "{instanceId:'%s',mountpoint:'/'}" % instanceId,
+      RegionId: instanceRegion,
+      Length: 1000
+    }
+    metric_datum = service.DescribeMetricDatum parameters
+
+    expect(metric_datum).to have_key("Datapoints")
+    expect(metric_datum["Datapoints"]).to have_key("Datapoint")
+  end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,3 +5,10 @@ require 'yaml'
 def load_options
   {:access_key_id => ENV['ACCESS_KEY_ID'], :access_key_secret => ENV['ACCESS_KEY_SECRET']}
 end
+
+def load_ecs_instance_id
+  ENV['ECS_InstanceId']
+end
+def load_ecs_instance_region
+  ENV['ECS_InstanceRegion']
+end
