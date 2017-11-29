648514f0208eaff56cca7b51d9f50f965b2ce856
Liu Lantao <Lax@users.noreply.github.com>
2017-11-29 20:10:26 +0800

    update aliyun CMS (#4)

--- lib/aliyun/services/cms.rb
+++ lib/aliyun/services/cms.rb
@@ -4,12 +4,12 @@ module Aliyun
       "Aliyu CMS Service"
     end
     def self.endpoint
-      'http://metrics.aliyuncs.com/'
+      raise "Service Endpoint Missing! endpoint_url: http://metrics.\%{Region}.aliyuncs.com/"
     end
     def self.default_parameters
       {
         :Format=>"JSON",
-        :Version=>"2015-04-20",
+        :Version=>"2017-03-01",
         :SignatureMethod=>"HMAC-SHA1",
         :SignatureVersion=>"1.0"
       }
--- spec/aliyun_cms_spec.rb
+++ spec/aliyun_cms_spec.rb
@@ -1,32 +1,32 @@
 require 'spec_helper'
 
 describe Aliyun do
-  it 'can create aliyun CMS(metrics) service' do
+  before do
     options = load_options
     options[:service] = :cms
-    service = Aliyun::Service.new options
+    options[:endpoint_url] = 'http://metrics.cn-beijing.aliyuncs.com/'
+    @service = Aliyun::Service.new options
+  end
 
-    expect(service).to be_instance_of(Aliyun::Service)
-    expect(service.service).to be(Aliyun::CMSConfig)
+  it 'can create aliyun CMS(metrics) service' do
+    expect(@service).to be_instance_of(Aliyun::Service)
+    expect(@service.service).to be(Aliyun::CMSConfig)
   end
 
   it 'can describe metric datum' do
-    options = load_options
-    options[:service] = :cms
-    service = Aliyun::Service.new options
     instanceId = load_ecs_instance_id
     instanceRegion = load_ecs_instance_region
     parameters = {
-      Namespace: 'acs/ecs',
-      MetricName: 'vm.DiskUtilization',
+      Project: 'acs_ecs_dashboard',
+      Metric: 'diskusage_utilization',
       StartTime: (Time.now - 180).utc.iso8601,
-      Dimensions: "{instanceId:'%s',mountpoint:'/'}" % instanceId,
+      Dimensions: "{instanceId:'%s',device:'/dev/xvda1'}" % instanceId,
       RegionId: instanceRegion,
       Length: 1000
     }
-    metric_datum = service.DescribeMetricDatum parameters
+    metric_datum = @service.QueryMetricList parameters
 
     expect(metric_datum).to have_key("Datapoints")
-    expect(metric_datum["Datapoints"]).to have_key("Datapoint")
+    expect(metric_datum["Datapoints"][0]).to have_key("diskname")
   end
 end
